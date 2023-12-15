b262e907799328bf6e67deb810c03f0e6f10169e
Thirakan Kunalai <thirakan_k@banpu.co.th>
2023-12-15 16:29:34 +0700

    refactor and add condition when input

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,47 @@
 import * as readline from 'readline';
 
+const regex = /^[\a]$/
 let times = 0
-let answer = ''
+let inputAnswer = ''
+let countTotal = 0
+let countPosition = 0
+let position: string[] = []
+let temp: string[] = []
 
-const checkCorrect = (answers: string[], strs: string[]) => {
-  let countTotal = 0
-  let countPosition = 0
-  const temp: string[] = []
+const checkAndCount = (str: string, strIndex: number, answer: string, answerIndex: number) => {
+  if (inputAnswer.includes(str)) {
+    temp.push(str)
+  }
+  if (answer === str && strIndex === answerIndex) {
+    position.push((strIndex + 1).toString())
+    countPosition++
+  }
+  countTotal = [...new Set(temp)].length
+}
 
-  strs.forEach((str, strIndex) =>
-    answers.forEach((answer, answerIndex) => {
-      if (answer.includes(str)) {
-        temp.push(str)
-      }
-      if (answer === str && strIndex === answerIndex) {
-        countPosition++
-      }
-    })
+const checkCorrect = (answers: string[], input: string[]) => {
+  countTotal = 0
+  countPosition = 0
+  position.length = 0
+  temp.length = 0
+
+  input.forEach((str, strIndex) =>
+    answers.forEach((answer, answerIndex) => checkAndCount(str, strIndex, answer, answerIndex))
   )
 
-  countTotal = [...new Set(temp)].length
   times++
 
-  console.table({
-    'Answer': answers.join(''),
-    'Random String': strs.join(''),
-    'Count Total': countTotal,
-    'Count Position': countPosition,
-    'Times': times
-  })
+  const value = [
+    {
+      'Answer': inputAnswer,
+      'Input String': input.join(''),
+      'Count Total': countTotal,
+      'Count Position': countPosition,
+      'Position': position.length > 0 ? position.join(',') : '',
+      'Times': times
+    }
+  ]
+  console.table(value, ['Answer', 'Input String', 'Count Total', 'Count Position', 'Position', 'Times'])
 }
 
 const readLine = readline.createInterface({
@@ -37,12 +50,17 @@ const readLine = readline.createInterface({
 });
 
 const inputString = () => {
+  console.log("If you want to exit, please press 'e'")
+
   readLine.question('Enter your string: ', (input) => {
     if (input === 'e') {
       readLine.close();
-    } else {
+    } else if (regex.test(input) && input.length === 4) {
       const str = [...input.toUpperCase()]
-      checkCorrect([...answer], str)
+      checkCorrect([...inputAnswer], str)
+      inputString()
+    } else {
+      console.log('Please enter a-z or A-Z and fill 4 characters')
       inputString()
     }
   })
@@ -50,12 +68,16 @@ const inputString = () => {
 
 const getInput = () => {
   console.log("If you want to exit, please press 'e'")
-  readLine.question('Enter your answer: ', (inputAnswer) => {
-    if (inputAnswer === 'e') {
+
+  readLine.question('Enter your answer: ', (input) => {
+    if (input === 'e') {
       readLine.close();
-    } else {
-      answer = inputAnswer.toLocaleUpperCase()
+    } else if (regex.test(input) && input.length === 4) {
+      inputAnswer = input.toLocaleUpperCase()
       inputString()
+    } else {
+      console.log('Please enter a-z or A-Z and fill 4 characters')
+      getInput()
     }
   });
 }
